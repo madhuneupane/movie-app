51039ef547890cad06c162058c9be3360a39b68f
madhuneupane <nyoupanemadhu07@gmail.com>
2023-09-29 17:59:44 -0700

    feat: fetching popular movies and movie with id

--- src/backend/apiGetData.js
+++ src/backend/apiGetData.js
@@ -1,28 +1,37 @@
 import { AUTH_KEY } from "../configuration/apidata";
-
-export const getPopularMovies = async () => {
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: AUTH_KEY,
-    },
-  };
-
-  try {
-    const response = await fetch(
-      'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1',
-      options
-    );
-
-    if (!response.ok) {
-      throw new Error('Failed to fetch data');
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: AUTH_KEY,
+  },
 };
+export const getMovies = async (param) => {
+  let movies = []
+  await fetch( `https://api.themoviedb.org/3/movie/${param}?language=en-US&page=1`, options)
+  .then(response => response.json())
+  .then(response => {
+    movies = response.results
+    
+  })
+  .catch(err => console.error(err));
+
+  return movies
+};
+export const getMovie = async (id) => {
+  //console.log(id);
+  let movies = {}
+  
+  await fetch( `https://api.themoviedb.org/3/movie/${id}?language=en-US&page=1`, options)
+  .then(response => response.json())
+  .then(response => {
+   
+    movies = response
+    //console.log(response);
+    
+  })
+  .catch(err => console.error(err));
+
+  return movies
+};
+
